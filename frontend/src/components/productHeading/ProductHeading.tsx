17d92c26b081dafe33e60d99d97ff74ab8fc7d2a
Kishan Kumar <rajkishan7870@gmail.com>
2025-03-16 02:18:39 +0530

    add category page

--- frontend/src/components/productHeading/ProductHeading.tsx
+++ frontend/src/components/productHeading/ProductHeading.tsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import style from "./ProductHeading.module.css"
+import { useNavigate } from 'react-router-dom'
 
 const ProductHeading = () => {
-  return (
-    <div className={style.productHeading}>
-        <div className={style.productTypo}>
-          Products
+
+    const navigate = useNavigate();
+
+    const handleAddCategory = () => {
+        navigate("/addcat")
+    }
+
+    const handleAddProduct = () => {
+
+    }
+
+    return (
+        <div className={style.productHeading}>
+            <div className={style.productTypo}>
+                Products
+            </div>
+            <div className={style.buttonContainer}>
+                <span>
+                    <button className={style.addCategory} onClick={handleAddCategory}>
+                        Add Category
+                    </button>
+                </span>
+                <span>
+                    <button className={style.addProduct} onClick={handleAddProduct}>
+                        Add Product
+                    </button>
+                </span>
+            </div>
         </div>
-        <div className={style.buttonContainer}>
-          <span><button className={style.addCategory}>Add Category</button></span>
-          <span><button className={style.addProduct}>Add Product</button></span>
-        </div>
-      </div>
-  )
+    )
 }
 
 export default ProductHeading
