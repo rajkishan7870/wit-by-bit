bfdb8983a82af1a6f77e69e98c5251657ef369f3
Kishan Kumar <rajkishan7870@gmail.com>
2025-03-16 01:06:29 +0530

    category layput with dummy data

--- frontend/src/components/categories/Categories.tsx
+++ frontend/src/components/categories/Categories.tsx
@@ -1,10 +1,66 @@
 import React from 'react'
 import style from "./Categories.module.css"
+import { FaIndianRupeeSign } from "react-icons/fa6";
 
 const Categories = () => {
-  return (
-    <div>Categories</div>
-  )
+    return (
+        <div className={style.allCatContainer}>
+            <div className={style.singleCatContainer}>
+                <div className={style.catName}>
+                    Shoes
+                </div>
+                <div className={style.productListContainer}>
+                    <div className={style.singleProductContainer}>
+                        <img src='https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/450ed1df-8e17-4d87-a244-85697874661c/NIKE+REVOLUTION+7.png' alt='im' />
+                        <div className={style.productDetails}>
+                            <div>
+                                <p>Nike Air Jordan</p>
+                                <p>12000</p>
+                            </div>
+                            <div className={style.brandName}>Nike</div>
+                        </div>
+                    </div>
+                    <div className={style.singleProductContainer}>
+                        <img src='https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/450ed1df-8e17-4d87-a244-85697874661c/NIKE+REVOLUTION+7.png' alt='im' />
+                        <div className={style.productDetails}>
+                            <div>
+                                <p>Nike Duck Low</p>
+                                <p>8000</p>
+                            </div>
+                            <div className={style.brandName}>Nike</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={style.singleCatContainer}>
+                <div className={style.catName}>
+                    T-Shirt
+                </div>
+                <div className={style.productListContainer}>
+                    <div className={style.singleProductContainer}>
+                        <img src='https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/450ed1df-8e17-4d87-a244-85697874661c/NIKE+REVOLUTION+7.png' alt='im' />
+                        <div className={style.productDetails}>
+                            <div>
+                                <p>Nike Air Jordan</p>
+                                <p>12000</p>
+                            </div>
+                            <div className={style.brandName}>Nike</div>
+                        </div>
+                    </div>
+                    <div className={style.singleProductContainer}>
+                        <img src='https://static.nike.com/a/images/t_PDP_936_v1/f_auto,q_auto:eco/450ed1df-8e17-4d87-a244-85697874661c/NIKE+REVOLUTION+7.png' alt='im' />
+                        <div className={style.productDetails}>
+                            <div>
+                                <p>Nike Duck Low</p>
+                                <p>8000</p>
+                            </div>
+                            <div className={style.brandName}>Nike</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Categories
