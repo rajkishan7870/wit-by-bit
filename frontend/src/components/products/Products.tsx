09c31ca2c61164d23307a0036c76056f0d6d49be
Kishan Kumar <rajkishan7870@gmail.com>
2025-03-15 23:08:49 +0530

    product heading

--- frontend/src/components/products/Products.tsx
+++ frontend/src/components/products/Products.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import style from "./Products.module.css"
 
 interface Props {}
 
 const Products : React.FC<Props> = () => {
   return (
-    <div>Products</div>
+    <div>
+      <div className={style.productHeading}>
+        <div className={style.productTypo}>
+          Products
+        </div>
+        <div>
+          <span><button className={style.addCategory}>Add Category</button></span>
+          <span><button className={style.addProduct}>Add Product</button></span>
+        </div>
+      </div>
+    </div>
   )
 }
 
